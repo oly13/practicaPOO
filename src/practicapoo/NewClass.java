2abe0302917a775f421d55b5bc9eac6301b136ab
oly13 <d.oliver13@me.com>
2015-12-28 19:30:55 +0100

    Merge origin/master

---
+++ src/practicapoo/NewClass.java
@@ -0,0 +1,24 @@
+/**
+*******************************
+    Autor: Daniel Oliver
+    practicapoo
+*******************************
+*/
+
+package practicapoo;
+/**Clase NewClass*/
+public class NewClass {
+    /**atributos*/
+    
+
+    /**Constructores*/
+    
+
+    /**GET y SET*/
+
+
+    /**Metodos*/
+
+
+
+}
